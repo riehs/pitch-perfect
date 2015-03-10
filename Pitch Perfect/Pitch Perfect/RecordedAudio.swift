12ca6514e87e95df1d710e42fb932eda69e35c29
riehs <riehs@bc.edu>
2015-03-09 22:26:56 -0400

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Daniel Riehs on 3/6/15.
+//  Copyright (c) 2015 Daniel Riehs. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
